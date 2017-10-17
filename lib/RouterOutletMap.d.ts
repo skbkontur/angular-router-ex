5affc618106fcd7cb1dc8c1b0408eea3d31c4eab
buhv <buhv@skbkontur.ru>
2017-10-17 19:52:35 +0500

    ngc compile

--- lib/RouterOutletMap.d.ts
+++ lib/RouterOutletMap.d.ts
@@ -2,7 +2,7 @@ import { ComponentFactoryResolver, Type, Injector, ComponentRef } from "@angular
 import { RouteContext } from "./RouteContext";
 import { ResolvedRoute } from "./Config";
 export interface IRouterOutlet {
-    activate(route: ResolvedRoute, url: string, component: Type<any>, resolver: ComponentFactoryResolver, injector: Injector): Promise<IOutletActivationResult>;
+    activate(route: ResolvedRoute, url: string, component: Type<any>, resolver: ComponentFactoryResolver, injector: Injector, force?: boolean): Promise<IOutletActivationResult>;
     activatedComponent: ComponentRef<any>;
 }
 export interface IOutletActivationResult {
--- lib/directives/router-ex-outlet.component.d.ts
+++ lib/directives/router-ex-outlet.component.d.ts
@@ -1,5 +1,5 @@
-import { ComponentFactoryResolver, ComponentRef, EventEmitter, Injector, OnDestroy, ViewContainerRef, Type } from "@angular/core";
-import { RouterOutletMap, IRouterOutlet, IOutletActivationResult } from "../RouterOutletMap";
+import { ComponentFactoryResolver, ComponentRef, EventEmitter, Injector, OnDestroy, Type, ViewContainerRef } from "@angular/core";
+import { IOutletActivationResult, IRouterOutlet, RouterOutletMap } from "../RouterOutletMap";
 import { ResolvedRoute } from "../Config";
 import { RouteReuseCache } from "../RouteReuseCache";
 import { RouterScrollWrapper } from "../RouterScrollWrapper";
@@ -18,14 +18,14 @@ export declare class RouterExOutletComponent implements OnDestroy, IRouterOutlet
     private activated;
     private routeCtx;
     constructor(parentOutletMap: RouterOutletMap, location: ViewContainerRef, reuseCache: RouteReuseCache, scrollWrapper: RouterScrollWrapper, name: string);
-    ngOnDestroy(): void;
     readonly isActivated: boolean;
     readonly activatedComponent: ComponentRef<any>;
-    activate(route: ResolvedRoute, url: string, componentType: Type<any>, resolver: ComponentFactoryResolver, injector: Injector): Promise<IOutletActivationResult>;
+    activate(route: ResolvedRoute, url: string, componentType: Type<any>, resolver: ComponentFactoryResolver, injector: Injector, force?: boolean): Promise<IOutletActivationResult>;
+    ngOnDestroy(): void;
     /**
      * Replace current activated component with specified one
      */
-    private activateComponent(ref);
-    private deactivateCurrent();
+    private activateComponent(ref, routeCtx);
     private attach(ref, routeContext);
+    private deactivateCurrent();
 }
