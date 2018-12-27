d31f62c61e5129c0b872a323db8e376354e18dfe
Чураков Михаил Анатольевич <churakov@skbkontur.ru>
2018-12-27 16:46:04 +0500

    - added route reuse strategy to multiple-outlets.component

--- sample/app/multiple-outlets/multiple-outlets.component.ts
+++ sample/app/multiple-outlets/multiple-outlets.component.ts
@@ -2,13 +2,15 @@ import {ChangeDetectionStrategy, Component} from "@angular/core";
 import {Title} from "./title";
 import {XLargeDirective} from "./x-large";
 import {disabledGuards, enableGuards} from "./Guards";
+import {IReusableRouterComponent, ReuseRouteStrategy} from "../../../src";
 
 @Component({
     selector: 'multiple-outlets',
     template: '<br><br><br><a tid="multiple-outlets-another-outlet-link" [href]="\'/multiple-outlets/1\'">goto route in another outlet</a><p tid="multiple-outlets-default-outlet-content">Here is standart outlet content.</p>',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MultipleOutletsComponent {
+export class MultipleOutletsComponent implements IReusableRouterComponent {
+    reuseRouteStrategy = ReuseRouteStrategy.CACHEBACK;
 
     constructor() {
     }
